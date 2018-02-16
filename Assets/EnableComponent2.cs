8d4c90108b5fbd9e9d52a9b62619e8f7f223c242
zzazabs <zzazabs@gmail.com>
2018-02-16 11:41:51 +0200

    Enablle Component

---
+++ Assets/EnableComponent2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Gamekit2D
+
+{
+
+	public class EnableComponent2 : MonoBehaviour
+
+	{
+		private PlayerCharacter myplayercharacter;
+
+		void Start()
+		{
+			myplayercharacter = GetComponent<PlayerCharacter>();
+		}
+
+
+		void Update()
+		{
+            if (Input.GetKeyDown(KeyCode.Space)) ;
+			{
+				myplayercharacter.enabled = !myplayercharacter.enabled;
+			}
+		}
+	}
+}
